16e7d089d52e32db7baafa8fb7c8ccc87bd6914e
AdamAzuddin <05adamaz@gmail.com>
2023-05-01 14:22:33 +0800

    Can add new item to list and render new li for each item

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+var items = ["Buy Food","Cook Food","Eat Food"];
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   var today = new Date();
@@ -12,12 +16,20 @@ app.get("/", function (req, res) {
   var options = {
     weekday: "long",
     day: "numeric",
-    month: "long"
-  }
+    month: "long",
+  };
 
-  var day = today.toLocaleDateString("en-US", options)
+  
+  var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { typeOfDay: day });
+  res.render("list", { typeOfDay: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  var newItem = req.body.newItem;
+  items.push(newItem);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
