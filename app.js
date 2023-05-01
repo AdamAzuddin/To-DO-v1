7e2f98dcf23a6b58fd1e22c210ebd313379bd874
AdamAzuddin <05adamaz@gmail.com>
2023-05-01 16:27:53 +0800

    Export getDay and getDate module from date.js and require it in app. js. Also refactor all app.js and date.js

--- app.js
+++ app.js
@@ -2,31 +2,23 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+ "/date.js")
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDate()
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  let newItem = req.body.newItem;
+  const newItem = req.body.newItem;
   if (req.body.list === "Work") {
     workItems.push(newItem);
     res.redirect("/work");
